4799cfecbd5a6d47d192da75120aad3ed5564f7c
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2025-04-26 20:18:49 +0800

    Update tsm_two_node_flow_env.bat

--- tsm_two_node_flow_env.bat
+++ tsm_two_node_flow_env.bat
@@ -11,13 +11,19 @@ set "today=%year%-%month%-%day%"
 :: Step 2: Set file name with date
 set "FILENAME=bootstrap_%today%.json"
 
-:: Step 3: Generate bootstrap file
+:: Step 3: Two Node Installation - Specialized for flow environments
 tsm topology nodes get-bootstrap-file --file "c:\temp\%FILENAME%"
 
 tsm topology set-process -n node2 -pr backgrounder -c 4
+tsm topology set-process -n node1 -pr backgrounder -c 2
+tsm topology set-process -n node1 -pr vizportal -c 2
+tsm topology set-process -n node1 -pr vizqlserver -c 2
+tsm topology set-process -n node1 -pr dataserver -c 2
+tsm topology set-process -n node1 -pr cacheserver -c 2
 tsm topology set-process -n node2 -pr gateway -c 1
 tsm topology set-process -n node2 -pr flowprocessor -c 1
 tsm topology set-process -n node2 -pr floweditor  -c 1
+tsm topology set-process -n node1 -pr floweditor  -c 0
 tsm topology set-node-role -n node2 -r flows
 tsm topology set-node-role -n node1 -r no-flows
 tsm pending-changes apply
